3a0e5851fa12121929eea66eaacda9d5e8bccae7
Gina Garrido <jorginagc@gmail.com>
2021-04-28 17:34:40 +0200

    ejercicio de programación funcional fecha: 28-042021

---
+++ week3_course_python_III/day2_python_VIII/exercises/classes_import/object/ClaseHumano.py
@@ -0,0 +1,35 @@
+class Humano:
+    def __init__ (self, nombre, armadura,nivel = 57, ataque, ojos=2, piernas=2, dientes=32, salud=100):
+        self.ojos = ojos
+        self.piernas = piernas
+        self.dientes = clientes
+        self.nombre = nombre
+        self.armadura = armadura
+        self.nivel = nivel  
+        self.ataque = ataque
+        self.salud = salud
+
+    def atacar(orco):
+        ataque = self.ataque - orco.armadura
+        vida_orco-restante = orco.salud - ataque
+        print (f'la vida restante del {orco} es: {vida_orco_restante}')
+        return vida_orco
+
+    def no-vivo():
+        if self.salud <= 0:
+            return True
+        else:
+            return False
+
+    def mostrar():
+        print (f' Nombre: {self.nombre} | ojos: {self.ojos}| dientes: {self.dientes} | piernas: {self.piernas} | armadura: {self.armadura} | nivel: {self.nivel} | salud: {self.salud}| ataque: {self.ataque}')
+
+
+
+
+
+	
+
+
+
+        
---
+++ week3_course_python_III/day2_python_VIII/exercises/classes_import/object/ClaseOrco.py
@@ -0,0 +1,26 @@
+lass Orco:
+    def __init__ (self, nombre, armadura,nivel = 38, ataque, ojos=2, piernas=2, dientes=56, salud=100):
+        self.ojos = ojos
+        self.piernas = piernas
+        self.dientes = clientes
+        self.nombre = nombre
+        self.armadura = armadura
+        self.nivel = nivel
+        self.ataque = ataque
+        self.salud = salud
+
+    def atacar(humano):
+        ataque = self.ataque - humano.armadura
+        vida_humano-restante = humano.salud - ataque
+        print (f'la vida restante del {humano} es: {vida_humano_restante}')
+        return vida_humano
+
+    def no-vivo():
+        if self.salud <= 0:
+            return True
+        else:
+            return False
+
+    def mostrar():
+        print (f' Nombre: {self.nombre} | ojos: {self.ojos}| dientes: {self.dientes} | piernas: {self.piernas} | armadura: {self.armadura} | nivel: {self.nivel} | salud: {self.salud}| ataque: {self.ataque}')
+
