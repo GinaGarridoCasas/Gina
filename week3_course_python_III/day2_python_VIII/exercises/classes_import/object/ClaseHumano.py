e76472cf92aebae7f5c2377748f80d39e6010dc3
Gina Garrido <jorginagc@gmail.com>
2021-04-29 18:57:25 +0200

    Class import orcos y humanos. 29-04-2021

--- week3_course_python_III/day2_python_VIII/exercises/classes_import/object/ClaseHumano.py
+++ week3_course_python_III/day2_python_VIII/exercises/classes_import/object/ClaseHumano.py
@@ -1,33 +1,33 @@
 class Humano:
-    def __init__ (self, nombre, armadura,nivel = 57, ataque, ojos=2, piernas=2, dientes=32, salud=100):
+    def __init__ (self, nombre, armadura, ataque, nivel = 57, ojos=2, piernas=2, dientes=32, salud=100):
         self.ojos = ojos
         self.piernas = piernas
-        self.dientes = clientes
+        self.dientes = dientes
         self.nombre = nombre
         self.armadura = armadura
         self.nivel = nivel  
         self.ataque = ataque
         self.salud = salud
 
-    def atacar(orco):
+    def atacar(self, orco):
         ataque = self.ataque - orco.armadura
-        vida_orco-restante = orco.salud - ataque
-        print (f'la vida restante del {orco} es: {vida_orco_restante}')
-        return vida_orco
+        orco.salud = orco.salud - self.ataque
+        print (f'la vida restante del {orco.nombre} es: {orco.salud}')
+        return orco.salud
 
-    def no-vivo():
+    def novivo(self):
         if self.salud <= 0:
             return True
         else:
             return False
 
-    def mostrar():
+    def mostrar(self):
         print (f' Nombre: {self.nombre} | ojos: {self.ojos}| dientes: {self.dientes} | piernas: {self.piernas} | armadura: {self.armadura} | nivel: {self.nivel} | salud: {self.salud}| ataque: {self.ataque}')
 
 
 
 
-
+C:\Users\ggarr\OneDrive\Escritorio\Data_Science\The Bridge\Gina\week3_course_python_III\day2_python_VIII\exercises\classes_import\object\ClaseHumano.py
 	
 
 
--- week3_course_python_III/day2_python_VIII/exercises/classes_import/object/ClaseOrco.py
+++ week3_course_python_III/day2_python_VIII/exercises/classes_import/object/ClaseOrco.py
@@ -1,26 +1,26 @@
-lass Orco:
-    def __init__ (self, nombre, armadura,nivel = 38, ataque, ojos=2, piernas=2, dientes=56, salud=100):
+class Orco:
+    def __init__ (self, nombre, armadura, ataque, nivel = 38, ojos=2, piernas=2, dientes=56, salud=100):
         self.ojos = ojos
         self.piernas = piernas
-        self.dientes = clientes
+        self.dientes = dientes
         self.nombre = nombre
         self.armadura = armadura
         self.nivel = nivel
         self.ataque = ataque
         self.salud = salud
 
-    def atacar(humano):
+    def atacar(self, humano):
         ataque = self.ataque - humano.armadura
-        vida_humano-restante = humano.salud - ataque
-        print (f'la vida restante del {humano} es: {vida_humano_restante}')
-        return vida_humano
+        humano.salud = humano.salud - self.ataque
+        print (f'la vida restante del {humano.nombre} es: {humano.salud}')
+        return humano.salud
 
-    def no-vivo():
+    def novivo(self):
         if self.salud <= 0:
             return True
         else:
             return False
 
-    def mostrar():
+    def mostrar(self):
         print (f' Nombre: {self.nombre} | ojos: {self.ojos}| dientes: {self.dientes} | piernas: {self.piernas} | armadura: {self.armadura} | nivel: {self.nivel} | salud: {self.salud}| ataque: {self.ataque}')
 
