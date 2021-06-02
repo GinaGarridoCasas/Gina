2b718e84169415cd8b4bd19bb02d95124a15fe2c
Gina Garrido <jorginagc@gmail.com>
2021-06-02 12:41:17 +0200

    Presentación EDA, readme, infojson

--- TICS_SALUD_EDA/info_json.py
+++ TICS_SALUD_EDA/info_json.py
@@ -2,16 +2,15 @@ import json
 import os
 
 data = {}
-data['nombre_proyecto'] = '¿Son más saludables las poblaciones con mejores conexiones a TICS?'
+data['nombre_proyecto'] = '¿Son más saludables en España las poblaciones con mejores conexiones a TICS?'
 data['autora'] = 'Jorgina Garrido Casas'
 data['linkedin'] = ''
-data['mail'] = ''
+data['mail'] = 'jorginagc@gmail.com'
 data['Título_proyecto'] = '¿Son más saludables las poblaciones con mejores conexiones a TICS?'
-data['Descripción_proyecto'] = 'El objetivo es conocer la asociación entre el tipo de conexión a TICS y la salud de la población. Se encuentran asociaciones positivas entre mayor conexión por cable o fibra óptica y dolor de espalda crónico cervical y lumbar, migraña, depresión y otros problemas de salud mental. Esta asociación está modulada por variables intermedias diferentes a: renta media del hogar, nivel de estudios, tasa de paro, índice de masculinidad o edad entre 25 y 59 años que sería de interés estudiar en futuros estudios, tales como habilidades digitales, ocupación mayoritaria de la población, ...
- '
+data['Descripción_proyecto'] = 'El objetivo es conocer la asociación entre el tipo de conexión a TICS y la salud de la población. Se encuentran asociaciones positivas entre mayor conexión por cable o fibra óptica y dolor de espalda crónico cervical y lumbar, migraña, depresión y otros problemas de salud mental. Esta asociación está modulada por variables intermedias diferentes a: renta media del hogar, nivel de estudios, tasa de paro, índice de masculinidad o edad entre 25 y 59 años que sería de interés estudiar en futuros estudios, tales como habilidades digitales, ocupación mayoritaria de la población, ...'
 data['flask_port'] = ''
 data['streamlit_port'] = ''
-data['python_libraries_used'] = 'pandas, numpys, matplotlib, seaborn'
+data['python_libraries_used'] = 'pandas, numpys, matplotlib, seaborn' 
 
 
 dir = 'C:\Users\ggarr\OneDrive\Escritorio\Data_Science\The Bridge\Gina\TICS_SALUD_EDA'  
@@ -19,4 +18,5 @@ file_name = "info.json"
 
 with open(dir, 'w+') as outfile:
     json.dump(file_name, outfile, indent=4)
-    
+
+  
